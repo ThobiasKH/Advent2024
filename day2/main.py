a095ce6206845482b5050bcfc46a18a81e5ff37b
ThobiasKH <pr.stfu@gmail.com>
2024-12-03 00:27:00 +0100

    Day 2

---
+++ day2/main.py
@@ -0,0 +1,25 @@
+data = []
+with open("day2/input.txt", "r") as file:
+    lines = file.readlines()
+    for line in lines:
+        x = line.strip().split()
+        print(x)
+        data.append([int(n) for n in x])
+
+file.close()
+
+def isSafe(report):
+    isIncreasing = report[0] <= report[1]
+    for i in range(len(report) - 1):
+        diff = report[i + 1] - report[i]
+        if abs(diff) > 3 or abs(diff) < 1:
+            return False
+        if isIncreasing and diff < 0 or not isIncreasing and diff > 0:
+            return False
+    return True
+
+numSafe = 0
+for i in range(len(data)):
+    numSafe += 1 if isSafe(data[i]) else 0
+
+print(numSafe)
