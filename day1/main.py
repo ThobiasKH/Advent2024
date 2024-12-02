60e5349b0c7c9f17ef89bb8cf1125dec3f409ec5
ThobiasKH <pr.stfu@gmail.com>
2024-12-03 00:17:41 +0100

    Day 1 complete

---
+++ day1/main.py
@@ -0,0 +1,26 @@
+list1 = []
+list2 = []
+
+with open("day1/input.txt", "r") as file:
+    lines = file.readlines()
+    for line in lines:
+        l = line.strip().split()
+        list1.append( int( l[0] ))
+        list2.append( int( l[1] ))
+
+file.close()
+
+dst = 0
+# slight misunderstanding
+#for i in range(1000):
+ #   l = sorted([int(digit) for digit in str(list1[i])])
+  #  r = sorted([int(digit) for digit in str(list2[i])])
+   # for j in range(len(l)):
+    #    dst += abs(l[j] - r[j])
+     
+list1.sort()
+list2.sort()
+for i in range(1000): 
+    dst += abs(list1[i] - list2[i])
+       
+print(dst)
--- main.py
+++
@@ -1,8 +0,0 @@
-list1 = []
-list2 = []
-
-with open("./input.txt", "r") as file:
-    for line in file:
-        print(line)
-
-file.close()
